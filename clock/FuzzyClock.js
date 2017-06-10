df6409d984532e823122c687bf59a6026a32e425
Scott <scottmtraver@gmail.com>
2017-06-10 11:53:47 -0600

    Basic Fuzzyness

--- clock/FuzzyClock.js
+++ clock/FuzzyClock.js
@@ -4,14 +4,31 @@
 
 var fuzzyMap = require('./mapping.json');
 var moment = require('moment');
+
+function round5(x) {
+    return (Math.ceil(x/5)*5)%30;
+}
+
+function aboutPastHalf(minutes) {
+    return (minutes >= 26);
+}
+
 var fuzzyClock = {
     getTime: function (timezone) {
         if(timezone) {
             // convert if applicable
         }
+        var min = new moment().minute();
         var hour = new moment().hour();
-        console.log(hour);
-        return 'About ' + fuzzyMap[hour];
+        var string = 'O clock';
+        if(aboutPastHalf(min) && min <= 55) {
+            string = 'thirty';
+        }
+        if(min >= 55) {
+            hour = hour + 1;
+        }
+        string = (round5(min) ? fuzzyMap.minutes[round5(min)] + ' past ' : 'About ') + fuzzyMap.hours[hour] + ' ' + string;
+        return string;
     }
 }
 
