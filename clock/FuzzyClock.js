144ac37f78080bf4d9eb28b66c5fa44f95c59853
Scott <scottmtraver@gmail.com>
2017-06-10 16:04:34 -0600

    Working rounded hour

--- clock/FuzzyClock.js
+++ clock/FuzzyClock.js
@@ -5,30 +5,14 @@
 var fuzzyMap = require('./mapping.json');
 var moment = require('moment');
 
-function round5(x) {
-    return (Math.ceil(x/5)*5)%30;
-}
-
-function aboutPastHalf(minutes) {
-    return (minutes >= 26);
-}
-
 var fuzzyClock = {
     getTime: function (timezone) {
         if(timezone) {
             // convert if applicable
         }
-        var min = new moment().minute();
-        var hour = new moment().hour();
-        var string = 'O clock';
-        if(aboutPastHalf(min) && min <= 55) {
-            string = 'thirty';
-        }
-        if(min >= 55) {
-            hour = hour + 1;
-        }
-        string = (round5(min) ? fuzzyMap.minutes[round5(min)] + ' past ' : 'About ') + fuzzyMap.hours[hour] + ' ' + string;
-        return string;
+        var hour = new moment().hour() + 1;
+        // return string;
+        return 'about ' + fuzzyMap.hours[hour%12];
     }
 }
 
